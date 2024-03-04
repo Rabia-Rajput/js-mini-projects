03467371b39ea7ad5395a11a15bf744c5fa804db
Rabia-Rajput <rajput.rabia@outlook.com>
2024-03-04 15:06:02 +0100

    new changes made

--- colorchange.js
+++ colorchange.js
@@ -1,2 +1,6 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  console.log(button);
+});
